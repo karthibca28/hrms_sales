3961cee8506f7978faa5348d6e7042d584e25cca
Godwin <godw335@gmail.com>
2024-02-14 15:27:51 +0530

    dashboard

--- src/app/pages/dashboards/dashboard/dashboard.component.ts
+++ src/app/pages/dashboards/dashboard/dashboard.component.ts
@@ -6,6 +6,28 @@ import { circle, latLng, tileLayer } from 'leaflet';
 
 import { BestSelling, TopSelling, RecentSelling, statData } from './data';
 import { ChartType } from './dashboard.model';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexXAxis,
+  ApexLegend,
+  ApexFill
+} from "ng-apexcharts";
+
+interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  responsive: ApexResponsive[];
+  xaxis: ApexXAxis;
+  legend: ApexLegend;
+  fill: ApexFill;
+};
 
 @Component({
   selector: 'app-dashboard',
@@ -17,6 +39,8 @@ import { ChartType } from './dashboard.model';
  * Ecommerce Component
  */
 export class DashboardComponent implements OnInit {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: ChartOptions;
 
   // bread crumb items
   breadCrumbItems!: Array<{}>;
@@ -37,6 +61,60 @@ export class DashboardComponent implements OnInit {
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     this.currentDate = { from: firstDay, to: lastDay }
+    this.chartOptions = {
+      series: [
+        {
+          name: "PRODUCT A",
+          data: [44, 55, 41, 67, 22, 43, 21, 49]
+        },
+        {
+          name: "PRODUCT B",
+          data: [13, 23, 20, 8, 13, 27, 33, 12]
+        },
+        {
+          name: "PRODUCT C",
+          data: [11, 17, 15, 15, 21, 14, 15, 13]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        stackType: "100%"
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: "bottom",
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }
+      ],
+      xaxis: {
+        categories: [
+          "2011 Q1",
+          "2011 Q2",
+          "2011 Q3",
+          "2011 Q4",
+          "2012 Q1",
+          "2012 Q2",
+          "2012 Q3",
+          "2012 Q4"
+        ]
+      },
+      fill: {
+        opacity: 1
+      },
+      legend: {
+        position: "right",
+        offsetX: 0,
+        offsetY: 50
+      }
+    } as ChartOptions;
   }
 
   ngOnInit(): void {
