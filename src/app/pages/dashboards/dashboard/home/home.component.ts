7d881c1caa12248133c73dab6455a355173d2aa9
Godwin <godw335@gmail.com>
2024-02-15 17:40:39 +0530

    changes

--- src/app/pages/dashboards/dashboard/home/home.component.ts
+++ src/app/pages/dashboards/dashboard/home/home.component.ts
@@ -1,10 +1,304 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexXAxis,
+  ApexLegend,
+  ApexFill,
+  ApexStroke,
+  ApexYAxis,
+  ApexTooltip,
+  ApexTitleSubtitle,
+  ApexGrid,
+  ApexNonAxisChartSeries
+} from "ng-apexcharts";
 
+interface sideBarOption {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+interface barChartOption {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  legend: ApexLegend;
+};
+interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  responsive: ApexResponsive[];
+  xaxis: ApexXAxis;
+  legend: ApexLegend;
+  fill: ApexFill;
+  colors: string[]
+  title: ApexTitleSubtitle;
+};
+interface  areaChart {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: ChartOptions;
+  public areaChart:areaChart
+  public salesChart:sideBarOption
+  public barChart:barChartOption
+  public barChartLeast:barChartOption
 
+  constructor(){
+    this.chartOptions = {
+      series: [
+        {
+          name: "IMFS",
+          data: [44, 55, 41, 67, 22]
+        },
+        {
+          name: "Beer",
+          data: [44, 55, 41, 67, 22]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        stackType: "100%"
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: "bottom",
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }
+      ],
+      xaxis: {
+        categories: [
+          "Chennai",
+          "Coimbatore",
+          "Madurai",
+          "Salem",
+          "Trichy"
+        ]
+      },
+      title: {
+        text: "",
+        align: "left",
+        // floating: true
+      },
+      fill: {
+        opacity: 1
+      },
+      colors: [
+        "#2E65FE",
+        "#17C6FF",
+        "#F13536",
+      ],
+      legend: {
+        position: "right",
+        offsetX: 0,
+        offsetY: 10
+      }
+    } as ChartOptions;
+    this.areaChart = {
+      series: [
+        {
+          name: "series1",
+          data: [31, 40, 28, 51, 42, 109, 100]
+        },
+        {
+          name: "series2",
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "area"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth"
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z"
+        ]
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm"
+        }
+      }
+    };
+    this.salesChart = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        type: "donut"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  this.barChart = {
+    series: [
+      {
+        name: "Net Profit",
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        // endingShape: "rounded"
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"]
+    },
+    xaxis: {
+      categories: [
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct"
+      ]
+    },
+    yaxis: {
+      title: {
+        text: "$ (thousands)"
+      }
+    },
+    fill: {
+      opacity: 1
+    },
+    tooltip: {
+      y: {
+        formatter: function(val) {
+          return "$ " + val + " thousands";
+        }
+      }
+    }
+  } as barChartOption
+  this.barChartLeast = {
+    series: [
+      {
+        name: "Old Sales Data",
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+      },
+      {
+        name: "New Sales Data",
+        data: [34, 65, 67, 36, 71, 88, 93, 30, 36]
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        // endingShape: "rounded"
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"]
+    },
+    xaxis: {
+      categories: [
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct"
+      ]
+    },
+    yaxis: {
+      title: {
+        text: "$ (thousands)"
+      }
+    },
+    fill: {
+      opacity: 1
+    },
+    tooltip: {
+      y: {
+        formatter: function(val) {
+          return "$ " + val + " thousands";
+        }
+      }
+    }
+  } as barChartOption
+  }
 }
+
